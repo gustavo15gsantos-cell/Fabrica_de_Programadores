02957bd3193669a044844e282b8b6b338f77b2c6
gustavo15gsantos-cell <gustavo.15.gsantos@gmail.com>
2026-05-05 15:16:34 -0300

    Dicionarios
    Aula 10 professor Hebert

---
+++ aula10.py
@@ -0,0 +1,12 @@
+#dicionario = chave: valor
+print("inicio\n")
+
+pessoa = {"nome": "vanessa",
+          "idade":32,
+          "cor_cabelo": "marrom"}
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+pessoa["cor_cabelo"] = "loirona"
+pessoa["idade"] = 40
+pessoa["nome"] = "carla"
+
+print(pessoa["idade"], pessoa["cor_cabelo"], pessoa["nome"])
