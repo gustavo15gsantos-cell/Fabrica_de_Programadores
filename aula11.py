9415e4bcde0f890bf5bada8fc1c62ec65c009538
gustavo15gsantos-cell <gustavo.15.gsantos@gmail.com>
2026-05-07 15:01:08 -0300

    AULA 11
    Aula 11 07/05/2026 professor Hebert

---
+++ aula11.py
@@ -0,0 +1,14 @@
+numero = int(input("digite um número: ")) #recebe um numero inteiro
+inicio = int(input("digite o ínicio da tabuada; ")) #recebe um numero inteiro para a tabuada
+fim = int(input("digite o fim da tabuada: ")) #numero atá onde a atbuada vai
+
+for i in range(inicio, fim +1): #
+    print(f"{numero} x {i} = {numero * i } ") #mostra a tabuada
+
+
+    #------------------------------------------------------------------#
+    
+    #explicação
+    #lista = ["vanessa","carla"]
+    #for x in lista:
+        #print(f"olá {x}")1
